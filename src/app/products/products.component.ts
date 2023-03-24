181dfd7810e0fcbbd73e900ea6c2e67cc12b9372
Alberto Araújo <albertoafaj@hotmail.com>
2023-03-24 16:49:33 -0300

    feat: create products view

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { IProduct, products } from '../products';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent {
+  products: IProduct[] | undefined = products;
+  constructor() { }
+  ngOnInit(): void {
+    console.log(this.products);
+
+  }
+
+}
